69e71c828d74556ae17d56d3b57381cda9607e4b
Student <student@donation-4.local>
2019-11-04 16:26:39 -0600

    started project. basic css design, loaded js to DOM, 'buy tickets' not done.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,32 @@
-function logShowsToConsole() {
-    for (let i = 0; i < SHOWS.length; i++) {
-        let show = SHOWS[i];
-        console.log(show.artist);
-    }
+function showShows() {
+  const showsList = document.getElementById("list-of-shows");
+  for (let i = 0; i < SHOWS.length; i++) {
+    let show = SHOWS[i];
+    let showInformation = `<li class="list-item">
+    <p>
+    ${show.artist} -
+    ${show.date} at
+    ${show.show} for only
+    $${show.price}. Tickets:
+    <div class="counter">
+    <span id="ticket-count">${show.tickets}</span>
+    <button class="buy-button">Buy Now!</button></div>
+    </p>
+    </li>`;
+    showsList.insertAdjacentHTML("beforeend", showInformation);
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    logShowsToConsole();
+document.addEventListener("DOMContentLoaded", function() {
+  showShows();
 });
+
+const counters = document.querySelectorAll(".counter");
+
+for (const counter of counters) {
+  const count = counter.querySelector(".buy-button");
+  count.addEventListener("click", function subtract() {
+    const num = counter.querySelector(".ticket-count");
+    num.textContent = Number(num.textContent) - 1;
+  });
+}
